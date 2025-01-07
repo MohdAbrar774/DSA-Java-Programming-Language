4430712471ece533951c28d0f980d60a4373178e
Mohd Abrar <111478375+MASheikh774@users.noreply.github.com>
2025-01-07 12:50:13 +0530

    Update QueueQs.java
    remove the duplicate interLeave function

--- Queue/QueueQs.java
+++ Queue/QueueQs.java
@@ -42,21 +42,6 @@ public class QueueQs {
         }
     }
 
-    public static void interLeave(Queue<Integer> q){
-        Queue<Integer> firstHalf = new LinkedList<>();
-
-        int size   = q.size();
-
-        for(int i=0; i<size/2; i++){
-            firstHalf.add(q.remove());
-        }
-        
-        while (!firstHalf.isEmpty()) {
-            q.add(firstHalf.remove());
-            q.add(q.remove());
-        }
-    }
-
     public static void reversQueue(Queue<Integer> q){
         Stack<Integer> s  = new Stack<>();
 
