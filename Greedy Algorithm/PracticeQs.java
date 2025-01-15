2cbd47261eff31ea9462fde43b9ebf5ceb2de94f
MASheikh774 <abrarsheikh774@gmail.com>
2025-01-15 11:29:46 +0530

    modified:   Greedy Algorithm/PracticeQs.java  Qs.02) Nth Odd Num In Range

--- Greedy Algorithm/PracticeQs.java
+++ Greedy Algorithm/PracticeQs.java
@@ -1,42 +1,73 @@
 public class PracticeQs {
 
-    
+    public static int maxEqualPartition(String str) {// O(n)
+        int n = str.length();
 
-    public static int maxEqualPartition(String str){//O(n)
-        int n=str.length();
+        int ans = 0;
+        int r = 0, l = 0;
 
-        int ans=0;
-        int r =0 , l = 0;
+        for (int i = 0; i < n; i++) {
 
-        for(int i=0; i<n; i++){
-
-            if( str.charAt(i) == 'R'){
+            if (str.charAt(i) == 'R') {
 
                 r++;
-            }
-            else if (str.charAt(i) == 'L') {
-                
+            } else if (str.charAt(i) == 'L') {
+
                 l++;
             }
 
-            if (r == l ) {
+            if (r == l) {
 
                 ans++;
-                
+
             }
         }
-         return ans;
+        return ans;
     }
 
+    public static int nthOddNumInRange(int range[], int K){
 
+        if (K <= 0) {
+            return 0;
+        }
 
+        int L = range[0];
+        int R = range[1];
 
+        if ((R&1) > 0) {
+            
+            int count = (int) Math.ceil((R-L+2)/2);
+
+            if (K > count) {
+                return 0;
+            }else{
+                return (R-2*K+2);
+            }
+
+        }
+        else{
+
+            int count =(R-L+2)/2;
+
+            if (K > count) {
+                return 0;
+            }else{
+                return (R-2*K+1);
+            }
+
+        }
+    }
 
     public static void main(String[] args) {
 
-        String str = "LRLLRRLRRL";
 
-       System.out.println(maxEqualPartition(str));
-        
+
+        int p[] = {-10, 10};
+        int k = 8;
+        System.out.println(nthOddNumInRange(p, k));
+        // String str = "LRLLRRLRRL";
+
+        // System.out.println(maxEqualPartition(str));
+
     }
 }
