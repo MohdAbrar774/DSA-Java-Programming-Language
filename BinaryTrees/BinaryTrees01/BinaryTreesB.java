e05bc9417319d96e6f515a8d5b761d7279409e0f
MASheikh774 <abrarsheikh774@gmail.com>
2025-01-16 23:22:05 +0530

    new file:   BinaryTrees/BinaryTrees01/BinaryTreesB.java PreOrder traversal

---
+++ BinaryTrees/BinaryTrees01/BinaryTreesB.java
@@ -0,0 +1,55 @@
+public class BinaryTreesB {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTrees {
+
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+
+            idx++;
+            if (nodes[idx] == -1) {
+
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+
+        public static void preOrder(Node root){
+            if (root == null) {
+                System.out.print(-1+" ");
+                return;
+            }
+            System.out.print(root.data+" ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTrees trees = new BinaryTrees();
+        Node root = trees.buildTree(nodes);
+        System.out.println(root.data);
+
+        trees.preOrder(root);
+
+    }
+}
