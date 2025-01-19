dca88862d8c5eae012b4b3a496fe7c202915dbdf
MASheikh774 <abrarsheikh774@gmail.com>
2025-01-19 12:49:38 +0530

    modified:   BinaryTrees/BinaryTrees01/BinaryTreesB.java Levelorder(1-n-2-3-n-4-5-6)

--- BinaryTrees/BinaryTrees01/BinaryTreesB.java
+++ BinaryTrees/BinaryTrees01/BinaryTreesB.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTreesB {
 
     static class Node {
@@ -61,6 +64,43 @@ public class BinaryTreesB {
             System.out.print(root.data+" ");
         }
 
+        public static void levelOrder(Node root) {
+            if (root == null) {
+
+                return;
+            }
+
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while (!q.isEmpty()) {
+                Node currNode = q.remove();
+                if (currNode == null) {
+
+                    System.out.println();
+                    if (q.isEmpty()) {
+
+                        break;
+                    } else {
+
+                        q.add(null);
+                    }
+                } else {
+
+                    System.out.print(currNode.data+" ");
+                    if (currNode.left != null) {
+
+                        q.add(currNode.left);
+                    }
+                     if(currNode.right != null){
+
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
+
     }
 
     public static void main(String[] args) {
@@ -69,7 +109,7 @@ public class BinaryTreesB {
         BinaryTrees trees = new BinaryTrees();
         Node root = trees.buildTree(nodes);
         System.out.println(root.data);
-        trees.postOrder(root);
+        trees.levelOrder(root);
 
     }
 }
