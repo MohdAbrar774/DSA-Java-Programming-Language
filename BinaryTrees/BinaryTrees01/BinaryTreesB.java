d77e6344b5c8f8b4c693a02b7f39bdbbd1d609cb
MASheikh774 <abrarsheikh774@gmail.com>
2025-01-19 12:57:34 +0530

    modified:   BinaryTrees/BinaryTrees01/BinaryTreesB.java (Height Of A Tree)

--- BinaryTrees/BinaryTrees01/BinaryTreesB.java
+++ BinaryTrees/BinaryTrees01/BinaryTreesB.java
@@ -102,14 +102,33 @@ public class BinaryTreesB {
         }
 
     }
+    public static int height(Node root){
+        if (root == null) {
+            
+            return 0;
+        }
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+
+        return Math.max(lh, rh)+1;
+    }
 
     public static void main(String[] args) {
 
-        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
-        BinaryTrees trees = new BinaryTrees();
-        Node root = trees.buildTree(nodes);
-        System.out.println(root.data);
-        trees.levelOrder(root);
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+        System.out.println(height(root));
+        
+        // int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        // BinaryTrees trees = new BinaryTrees();
+        // Node root = trees.buildTree(nodes);
+        // System.out.println(root.data);
+        // trees.levelOrder(root);
 
     }
 }
