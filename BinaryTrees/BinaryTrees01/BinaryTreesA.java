4bff1ce06dc145d32a5af34578fb64a7067e45e6
MASheikh774 <abrarsheikh774@gmail.com>
2025-01-23 12:24:43 +0530

    renamed:    BinaryTrees/BinaryTrees01/BinaryTreesB.java -> BinaryTrees/BinaryTrees01/BinaryTreesA.java 	renamed:    BinaryTrees/BinaryTrees02/BinaryTrees.java -> BinaryTrees/BinaryTrees02/BinaryTreeB.java 	new file:   BinaryTrees/BinaryTrees03/BinaryTreeC.java   Kth Level -> O(N)

--- BinaryTrees/BinaryTrees01/BinaryTreesA.java
+++ BinaryTrees/BinaryTrees01/BinaryTreesA.java
@@ -1,7 +1,7 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class BinaryTreesB {
+public class BinaryTreesA {
 
     static class Node {
         int data;
--- BinaryTrees/BinaryTrees02/BinaryTreeB.java
+++ BinaryTrees/BinaryTrees02/BinaryTreeB.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class BinaryTrees {
+public class BinaryTreeB {
 
     static class Node {
         int data;
