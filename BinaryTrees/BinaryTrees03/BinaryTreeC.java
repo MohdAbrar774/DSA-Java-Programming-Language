c9bab3838ebf5635a1b61cba3374f0e3d933cd0d
MASheikh774 <abrarsheikh774@gmail.com>
2025-01-27 12:00:15 +0530

    modified:   BinaryTrees/BinaryTrees03/BinaryTreeC.java  Min Distance Between two Nodes

--- BinaryTrees/BinaryTrees03/BinaryTreeC.java
+++ BinaryTrees/BinaryTrees03/BinaryTreeC.java
@@ -127,6 +127,34 @@ public class BinaryTreeC {
 
         return root;
     }
+
+    public static int lcaDist(Node root, int n){
+        if (root == null) {
+            return -1;
+        }
+        if (root.data == n) {
+            return 0;
+        }
+
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        }else if (leftDist == -1) {
+            return rightDist+1;
+        }else{
+            return leftDist+1;
+        }
+    }
+    public static int minDist(Node root, int n1, int n2){
+
+        Node lca = lca2(root, n1, n2);
+        int dist1 = lcaDist(lca, n1); 
+        int dist2 = lcaDist(lca, n2); 
+
+        return dist1 + dist2;
+    }
     public static void main(String[] args) {
         /*
          1
@@ -144,7 +172,7 @@ public class BinaryTreeC {
          root.right.left = new Node(6);
          root.right.right = new Node(7);
 
-         int n1 = 4, n2 = 6;
-        System.out.println(lca2(root, n1, n2).data);
+         int n1 = 2, n2 =3 ;
+        System.out.println(minDist(root, n1, n2));
     }
 }
