ef2aeabbabff664abfc3f8e489c586dc63c34bf6
MASheikh774 <abrarsheikh774@gmail.com>
2025-01-27 22:29:43 +0530

    modified:   BinaryTrees/BinaryTrees03/PracticeQues.java Que.02 --> Invert BT(Mirror Of BT)

--- BinaryTrees/BinaryTrees03/PracticeQues.java
+++ BinaryTrees/BinaryTrees03/PracticeQues.java
@@ -33,18 +33,44 @@ public static boolean isUnivalued(Node root) {
 return isUnivalued(root.left) &&  isUnivalued(root.right);
 
 }
+public static void mirrorTree(Node root){
 
+    if (root == null) {
+        return;
+    }
+
+    mirrorTree(root.left);
+    mirrorTree(root.right);
+
+    Node temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+}
+public static void preOrder(Node root) {
+    if (root == null) {
+        System.out.print(-1 + " ");
+        return;
+    }
+    System.out.print(root.data + " ");
+    preOrder(root.left);
+    preOrder(root.right);
+}
 
 
     public static void main(String[] args) {
         
         Node root = new Node(1);
-        root.left = new Node(1);     
-        root.right = new Node(1);
-        root.left.left = new Node(5);   
+        root.left = new Node(2);     
+        root.right = new Node(3);
+        root.left.left = new Node(4);   
+        root.left.right = new Node(5);   
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
     
+        mirrorTree(root);
+        preOrder(root);
        
-       System.out.println(isUnivalued(root));
+       //System.out.println(isUnivalued(root)); //Que.01
         
         
     }
