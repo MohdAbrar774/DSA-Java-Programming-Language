d631f473baebfa6e6712facc56953769741d08ca
MASheikh774 <abrarsheikh774@gmail.com>
2025-01-29 21:20:55 +0530

    modified:   BinaryTrees/BinaryTrees03/PracticeQues.java  Find Duplicate Subtree

--- BinaryTrees/BinaryTrees03/PracticeQues.java
+++ BinaryTrees/BinaryTrees03/PracticeQues.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class PracticeQues {
     static class Node {
         int data;
@@ -78,16 +80,49 @@ public class PracticeQues {
 
     }
 
+    static HashMap<String, Integer> m;
+
+    public static String inOrder(Node node) {
+        if (node == null) {
+            return "";
+        }
+
+        String str = "(";
+        str += inOrder(node.left);
+        str += Integer.toString(node.data);
+        str += inOrder(node.right);
+        str = ")";
+
+        if (m.get(str) != null && m.get(str) == 1) {
+
+            System.out.print(node.data + " ");
+        }
+        if (m.containsKey(str)) {
+            m.put(str, m.get(str) + 1);
+        } else {
+            m.put(str, 1);
+        }
+        return str;
+    }
+
+    public static void duplicateSubTree(Node root) {
+
+        m = new HashMap<>();
+        inOrder(root);
+    }
+
+    
+
     public static void main(String[] args) {
 
         Node root = new Node(1);
-        root.left = new Node(3);
+        root.left = new Node(2);
         root.right = new Node(3);
-        root.left.left = new Node(3);
-        root.left.right = new Node(2);
-
-        delLeafNode(root, 3);
-        preOrder(root);
+        root.left.left = new Node(4);
+        root.right.left = new Node(2);
+        root.right.left.left = new Node(4);
+        root.right.right = new Node(4);
+       duplicateSubTree(root);
 
     }
 }
