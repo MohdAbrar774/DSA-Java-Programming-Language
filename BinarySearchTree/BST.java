52f0512c997a038869c866cf1934a8ad32365d30
Mohd Abrar <111478375+MASheikh774@users.noreply.github.com>
2025-01-31 17:22:57 +0530

    Update BST.java   Print In Range

--- BinarySearchTree/BST.java
+++ BinarySearchTree/BST.java
@@ -88,11 +88,27 @@ public class BST {
         }
         return root;
     }
+    public static void printInRange(Node root, int k1, int k2){
+        if (root == null) {
+            return;
+        }
+        if (root.data >= k1 && root.data <= k2) {
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data+" ");
+            printInRange(root.right, k1, k2);
+        }
+        else if (root.data < k1) {
+            printInRange(root.left, k1, k2);
+        }else{
+            printInRange(root.right, k1, k2);
+            
+        }
+    }
   
   
 
     public static void main(String[] args) {
-        int values[] = {5, 1, 3, 4, 2, 7};
+        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
         Node root = null;
 
         for (int i = 0; i < values.length; i++) {
@@ -102,9 +118,9 @@ public class BST {
         inOrder(root);
         System.out.println();
         
-        delete(root, 5);
+       
         System.out.println();
-        inOrder(root);
+        printInRange(root, 5 ,12);
        
     }
 }
