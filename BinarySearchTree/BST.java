d47c662a073fb8e35531c0e60ae3a14a84fc9d49
Mohd Abrar <111478375+MASheikh774@users.noreply.github.com>
2025-02-02 23:45:02 +0530

    Update BST.java  Check Validation Of BST

--- BinarySearchTree/BST.java
+++ BinarySearchTree/BST.java
@@ -146,8 +146,19 @@ public static void printRoot2Leaf(Node root, ArrayList<Integer> path){
       printRoot2Leaf(root.right, path);
       path.remove(path.size()-1);
 }
-       
-  
+       public static boolean isValidBST(Node root, Node min, Node max){
+
+     if(root == null){
+            return true;
+     }
+     if(min != null  &&  root.data <= min.data){
+            return false;
+     }else if(max != null  && root.data  >= max.data){
+            return false;
+      }
+
+       return isValidBST(root.left, min, root)  &&  isValidBST(root.right, root, max);
+     }
   
 
     public static void main(String[] args) {
@@ -162,7 +173,7 @@ public static void printRoot2Leaf(Node root, ArrayList<Integer> path){
         System.out.println();
         
        
-        printRoot2Leaf(root, new ArrayList<>());
+        isValidBST(root,null,null);
        
     }
 }
