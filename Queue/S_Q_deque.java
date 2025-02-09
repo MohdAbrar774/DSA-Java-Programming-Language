cfba1f7ee1634605e39eade919c04f5ef8289514
MASheikh774 <abrarsheikh774@gmail.com>
2025-02-09 12:50:50 +0530

    new file:   Queue/S_Q_deque.java 	modified:   Queue/S_Q_deque.java     Stack Using DEQUE

---
+++ Queue/S_Q_deque.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class S_Q_deque {
+
+    static class Stack {
+        Deque<Integer> deque = new LinkedList<>();
+
+        public void push(int data) {
+            deque.addLast(data);
+        }
+
+        public int pop() {
+           return deque.removeLast();
+        }
+
+        public int peek() {
+          return  deque.getLast();
+        }
+    }
+    static class Queue{
+        Deque<Integer> deque = new LinkedList<>();
+
+        public void add(int data){
+            deque.addLast(data);
+        }
+        public int remove(){
+            return deque.removeFirst();
+        }
+        public int peek(){
+            return deque.getFirst();
+        }
+    }
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+    }
+
+}
