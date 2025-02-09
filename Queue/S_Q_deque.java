ef6822792e57b78eda8d877acc78debfd49fdf82
MASheikh774 <abrarsheikh774@gmail.com>
2025-02-09 12:58:32 +0530

    modified:   Queue/S_Q_deque.java    Queue Using DEQUE

--- Queue/S_Q_deque.java
+++ Queue/S_Q_deque.java
@@ -10,35 +10,48 @@ public class S_Q_deque {
         }
 
         public int pop() {
-           return deque.removeLast();
+            return deque.removeLast();
         }
 
         public int peek() {
-          return  deque.getLast();
+            return deque.getLast();
         }
     }
-    static class Queue{
+    static class Queue {
         Deque<Integer> deque = new LinkedList<>();
 
-        public void add(int data){
+        public void add(int data) {
             deque.addLast(data);
         }
-        public int remove(){
+
+        public int remove() {
             return deque.removeFirst();
         }
-        public int peek(){
+
+        public int peek() {
             return deque.getFirst();
         }
     }
     public static void main(String[] args) {
-        Stack s = new Stack();
-        s.push(1);
-        s.push(2);
-        s.push(3);
+       Queue q= new Queue();
+       q.add(1);
+       q.add(2);
+       q.add(3);
 
-        System.out.println(s.pop());
-        System.out.println(s.pop());
-        System.out.println(s.pop());
+       System.out.println(q.remove());
+       System.out.println(q.remove());
+       System.out.println(q.remove());
+ 
+       
+                        
+        // Stack s = new Stack();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
+
+        // System.out.println(s.pop());
+        // System.out.println(s.pop());
+        // System.out.println(s.pop());
     }
 
 }
