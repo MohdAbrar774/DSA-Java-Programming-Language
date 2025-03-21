c02eb91547aa3ba0adbd6789a66e64fb03bff72f
MASheikh774 <abrarsheikh774@gmail.com>
2025-03-21 11:33:29 +0530

    new file:   BinarySearchTree/BSTQue.java   SumInRange & Closest Element

---
+++ BinarySearchTree/BSTQue.java
@@ -0,0 +1,75 @@
+
+public class BSTQue {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+
+    }
+
+    public static int SumInRange(Node root, int k1, int k2) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.data >= k2) {
+            return SumInRange(root.left, k1, k2);
+        } else if (root.data < k1) {
+            return SumInRange(root.right, k1, k2);
+        }
+
+        int left = SumInRange(root.left, k1, k2);
+        int right = SumInRange(root.right, k1, k2);
+
+        return left + right + root.data;
+    }
+
+    public static int closest = 0;
+
+    public static void closestElem(Node root, int key) {
+        if (root == null) {
+            return;
+        }
+     
+        if (Math.abs(root.data - key) <  Math.abs(key- closest)) {
+            closest = root.data;
+        }
+        if (root.data > key) {
+            
+            closestElem(root.left, key);
+        } else{
+            closestElem(root.right, key);
+
+        }
+
+    }
+   
+    public static void main(String[] args) {
+        Node root = new Node(50);
+        root.left = new Node(30);
+        root.left.left = new Node(5);
+        root.left.right = new Node(45);
+
+        root.right = new Node(60);
+        root.right.right = new Node(70);
+        root.right.left = new Node(55);
+        root.right.right.right = new Node(80);
+
+          /*
+           50
+          /  \
+         30   60
+        /  \ /  \
+       5  45 55  70
+                   \
+                    80
+         */
+       closestElem(root, 56);
+       System.out.println(closest);
+    }
+}
