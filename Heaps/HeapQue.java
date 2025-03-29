6966c5037d2cb79100015c2b6ae4ebefb61250c8
Abrar <abrarsheikh774@gmail.com>
2025-03-29 12:29:02 +0530

    modified:   Heaps/HeapQue.java   Path with Min Effort

--- Heaps/HeapQue.java
+++ Heaps/HeapQue.java
@@ -66,14 +66,77 @@ public class HeapQue {
          System.out.println(time-1);
     }
 
-    
+    static class Info implements Comparable<Info>{
+        
+        int diff;
+         int x;
+         int y;
+
+         public Info(int diff, int x, int y){
+            this.diff = diff;
+            this.x = x;
+            this.y = y;
+
+         }
+
+         @Override
+         public int compareTo(Info n2){
+            return Integer.compare(this.diff, n2.diff);
+         }
+    }
+    public static int[][] dirs = {{0,1}, {1, 0}, {-1, 0}, {0, -1}};
+
+
+    public static int minEffortPath(int[][] height){
+        int m = height.length;
+        int n = height[0].length;
+
+        int[][] result = new int[m][n];
+        for (int[] row : result) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+
+        PriorityQueue<Info> pq = new PriorityQueue<>();
+        pq.offer(new Info(0, 0, 0));
+        result[0][0] = 0;
+
+        while (!pq.isEmpty()) {
+            Info node = pq.poll();
+            int diff = node.diff;
+            int x = node.x;
+            int y = node.y;
+
+            if (x == m - 1 && y == n - 1) {
+                return diff;
+            }
+
+            for (int[] dir : dirs) {
+                int x_ = x + dir[0];
+                int y_ = y + dir[1];
+
+                if (x_ >= 0 && x_ < m && y_ >= 0 && y_ < n) {
+                    int newDiff = Math.max(diff, Math.abs(height[x][y] - height[x_][y_]));
+                    if (result[x_][y_] > newDiff) {
+                        result[x_][y_] = newDiff;
+                        pq.offer(new Info(newDiff, x_, y_));
+                    }
+                }
+            }
+        }
+
+        return result[m - 1][n - 1];
+
+
+
+    }
     public static void main(String[] args) {
-        int arr[] = {2, 6};
-        int  k = 2;
-        int n = 6;
+      int cells[][] = {
+        {1, 2, 2},
+        {3, 8, 2},
+        {5, 3, 5}
+      };
 
-        minTimeToFillSlots(arr, k , n);
-
-
+      System.out.println(" "+minEffortPath(cells)+"  ");
+    
     }
 }
