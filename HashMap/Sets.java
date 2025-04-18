8dd6491fd976f969936180dc1b077ded25844b56
Abrar <abrarsheikh774@gmail.com>
2025-04-18 23:01:14 +0530

    modified:   HashMap/Sets.java   Count Distinct Numbers

--- HashMap/Sets.java
+++ HashMap/Sets.java
@@ -1,36 +1,17 @@
 import java.util.*;
 
 public class Sets {
-    
 
+    public static void main(String[] args) {//Count Distinct Numbers
+        int nums[] = { 4, 3, 2, 5, 6, 7, 3, 4, 2, 1 };
 
+        HashSet<Integer> hs = new HashSet<>();
 
- public static void main(String[] args) {
-    HashSet<String>  cities = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            hs.add(nums[i]);
+        }
 
-    cities.add("Delhi");
-    cities.add("Chennai");
-    cities.add("Nagpur");
-    cities.add("Mumbai");
-    cities.add("Nashik");
-    System.out.println(cities);
+        System.out.println("Distinct Numbers: " + hs.size());
 
-    LinkedHashSet<String> lhs = new LinkedHashSet<>();
-    lhs.add("Delhi");
-    lhs.add("Chennai");
-    lhs.add("Nagpur");
-    lhs.add("Mumbai");
-    lhs.add("Nashik");
-    System.out.println(lhs);
-
-    TreeSet<String> ts = new TreeSet<>();
-    ts.add("Delhi");
-    ts.add("Chennai");
-    ts.add("Nagpur");
-    ts.add("Mumbai");
-    ts.add("Nashik");
-    System.out.println(ts);
-
-
-}
+    }
 }
