0560913077cd50e4310a43e732a6aeca0c08e995
Abrar <abrarsheikh774@gmail.com>
2025-04-18 23:44:29 +0530

    modified:   HashMap/Sets.java   Union & Intersection of 2 arrays

--- HashMap/Sets.java
+++ HashMap/Sets.java
@@ -2,16 +2,40 @@ import java.util.*;
 
 public class Sets {
 
-    public static void main(String[] args) {//Count Distinct Numbers
-        int nums[] = { 4, 3, 2, 5, 6, 7, 3, 4, 2, 1 };
+    public static void main(String[] args) {//Union & Intersection of 2 arrays
+        int arr1[] = {7, 3, 9 };
+        int arr2[] = {6, 3, 9, 2, 9, 4};
 
-        HashSet<Integer> hs = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            hs.add(nums[i]);
+        for (int i = 0; i<arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+        for (int i = 0; i<arr2.length; i++) {
+            set.add(arr2[i]);
         }
 
-        System.out.println("Distinct Numbers: " + hs.size());
+        System.out.println("Union: " + set.size());
+        System.out.println();
+
+        //intersection
+
+        set.clear();
+        for (int i = 0; i<arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+
+        int count = 0;
+        for (int i = 0; i<arr2.length; i++) {
+          int num = arr2[i];
+            if(set.contains(num)){
+
+                count++;
+                set.remove(num);
+            }
+        }
+
+        System.out.println("Intersection: " + count);
 
     }
 }
