b631a9286c1a3c3abbd2ece3e2f970820c8ccbe0
Abrar <abrarsheikh774@gmail.com>
2025-04-20 13:10:29 +0530

    modified:   HashMap/HashMapA.java  Largest subArray with sum as 0

--- HashMap/HashMapA.java
+++ HashMap/HashMapA.java
@@ -47,21 +47,22 @@ public class HashMapA {
     }
 
     public static void main(String[] args) {
-        HashMap<String, String> tickets = new HashMap<>();
+        int arr[] = { 5, -2, 2, -8, 1, 7, 10, 23 };
 
-        tickets.put("Chennai", "Bengaluru");
-        tickets.put("Mumbai", "Delhi");
-        tickets.put("Goa", "Chennai");
-        tickets.put("Delhi", "Goa");
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        String start = getStart(tickets);
-        System.out.println();
-        System.out.print(start);
+        int sum = 0;
+        int len = 0;
 
-        for (String key : tickets.keySet()) {
-            System.out.print("-> " + tickets.get(start));
-            start = tickets.get(start);
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (map.containsKey(sum)) {
+                len = Math.max(len, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+
         }
-
+        System.out.println("Largest subArray with sum as  0: " + len);
     }
 }
