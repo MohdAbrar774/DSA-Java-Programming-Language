9b31bea9a5e5900767984ac8b32afaccd1c2471b
Abrar <abrarsheikh774@gmail.com>
2025-04-25 11:50:16 +0530

    modified:   Tries/TriesCode.java    Insert & Search in Tries

--- Tries/TriesCode.java
+++ Tries/TriesCode.java
@@ -12,9 +12,44 @@ public class TriesCode {
 
     public static Node root = new Node();
 
-   
+    public static void insert(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+
+            curr = curr.children[idx];
+        }
+
+        curr.eow = true;
+    }
+
+    public static boolean search(String key) {
+        Node curr = root;
+
+        for (int length = 0; length < key.length(); length++) {
+            int idx = key.charAt(length) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+
+        return curr.eow == true;
+    }
 
     public static void main(String[] args) {
 
+        String[] s = { "the", "a", "there", "their", "any" };
+
+        for (String str : s) {
+            insert(str);
+        }
+
+        System.out.println(search("any"));
+        System.out.println(search("an"));
     }
 }
