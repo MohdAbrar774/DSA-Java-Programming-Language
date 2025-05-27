46a77616395b6851cd40d33cce947f82c478c78a
Abrar <abrarsheikh774@gmail.com>
2025-05-27 20:02:43 +0530

    new file:   Graph/Graph - 2/Bipartite.java 	new file:   Graph/Graph - 2/Topological.java

---
+++ Graph/Graph - 2/Topological.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+public class Topological {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    public static void create(ArrayList<Edge>[] graph) {
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 0, 1));
+
+        graph[1].add(new Edge(1, 1, 1));
+
+        graph[2].add(new Edge(2, 3, 1));
+
+        graph[3].add(new Edge(3, 1, 1));
+
+        graph[4].add(new Edge(4, 0, 1));
+        graph[4].add(new Edge(4, 1, 1));
+
+        graph[4].add(new Edge(5, 0, 1));
+        graph[4].add(new Edge(5, 2, 1));
+
+    }
+
+    public static void topSort(ArrayList<Edge>[] graph) {
+        boolean[] vis = new boolean[graph.length];
+
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                topSortUtil(graph, i, vis, s);
+            }
+        }
+
+        while (!s.isEmpty()) {
+            System.out.print(s.pop() + " ");
+        }
+    }
+
+    public static void topSortUtil(ArrayList<Edge>[] graph, int curr, boolean[] vis, Stack<Integer> s) {
+        vis[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]) {
+                topSortUtil(graph, e.dest, vis, s);
+            }
+        }
+
+        s.push(curr);
+    }
+
+    public static void main(String[] args) {
+        int V = 6;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        create(graph);
+
+        topSort(graph);
+    }
+
+}
