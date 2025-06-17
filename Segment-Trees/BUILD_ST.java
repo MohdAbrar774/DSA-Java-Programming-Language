160c32f5bc1826d35ee12101d20cea75bf333e35
Abrar <abrarsheikh774@gmail.com>
2025-06-18 00:02:51 +0530

    new file:   3405.count-the-number-of-arrays-with-k-matching-adjacent-elements.java new file:   Segment-Trees/BUILD_ST.java

---
+++ Segment-Trees/BUILD_ST.java
@@ -0,0 +1,19 @@
+public class BUILD_ST{
+    static int[] tree;
+
+    public static void init(int n){
+        tree = new int[4*n];
+    }
+
+    public static int buildST(int[] arr, int i, int start, int end){
+        if(start == end){
+            tree[i] = arr[start];
+            return arr[start];
+        }
+
+
+
+
+return tree[i]l
+    }
+}
