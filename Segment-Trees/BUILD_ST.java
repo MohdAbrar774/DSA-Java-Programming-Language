28f7aa9383c87dc13c00a80abaf6bcbffdb6a585
Abrar <abrarsheikh774@gmail.com>
2025-06-18 12:18:27 +0530

    modified:   Segment-Trees/BUILD_ST.java 	new file:   Segment-Trees/QUERY_ON_ST.java 	new file:   Segment-Trees/UPDATE_ST.java

--- Segment-Trees/BUILD_ST.java
+++ Segment-Trees/BUILD_ST.java
@@ -1,8 +1,8 @@
-public class BUILD_ST{
+public class BUILD_ST {
     static int[] tree;
 
-    public static void init(int n){
-        tree = new int[4*n];
+    public static void init(int n) {
+        tree = new int[4 * n];
     }
 
     public static int buildST(int[] arr, int i, int start, int end){
@@ -11,9 +11,24 @@ public class BUILD_ST{
             return arr[start];
         }
 
+        int mid = (start+end)/2;
 
+        buildST(arr, 2*i+1, start, mid);
+        buildST(arr, 2*i+2,mid+1, end);
+        tree[i] = tree[2*i+1] +tree[2*i+2];
+        return tree[i];
 
+    }
 
-return tree[i]l
+    public static void main(String[] args) {
+        int arr[]  = {1, 2, 3, 4, 5, 6, 7, 8};
+        int  n= arr.length;
+        init(n);
+        buildST(arr, 0 , 0, n-1);
+
+        for(int i=0; i<n*4; i++){
+            System.out.print(tree[i]+" ");
+        }
+        System.out.println();
     }
 }
