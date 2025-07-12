b9a37a68bf2ab3166de5d1cdbb793dabc13da8a6
Abrar <abrarsheikh774@gmail.com>
2025-07-12 18:52:23 +0530

    M_C_M -  Tabulation

--- DP - Dynamic Programming/DP - 5/MATRIX_CHAIN_MULTIPLICATION.java
+++ DP - Dynamic Programming/DP - 5/MATRIX_CHAIN_MULTIPLICATION.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class MATRIX_CHAIN_MULTIPLICATION {
 
     public static int minCost(int arr[], int i, int j) {
@@ -41,17 +39,34 @@ public class MATRIX_CHAIN_MULTIPLICATION {
         return dp[i][j] = ans;
     }
 
+    public static int minCostTab(int arr[]) {
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+
+        // Initialization
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 0;
+        }
+
+        for (int len = 2; len <= n - 1; len++) {
+            for (int i = 1; i <= n - len; i++) {
+                int j = len + i - 1;// column
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k <= j - 1; k++) {
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k + 1][j];
+                    int cost3 = arr[i - 1] * arr[k] * arr[j];
+                    dp[i][j] = Math.min(dp[i][j], cost1 + cost2 + cost3);
+                }
+            }
+        }
+        return dp[1][n - 1];
+    }
+
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 4, 3 };
 
-        int n = arr.length;
-
-        int dp[][] = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-
-        System.out.println(minCostMemo(arr, 1, n - 1, dp));
+        System.out.println(minCostTab(arr));
 
     }
 
